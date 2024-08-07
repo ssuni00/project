a7963f4baffff6c94bba41d1486060f1a26295a1
ssuni00 <126534280+ssuni00@users.noreply.github.com>
2024-08-07 20:28:15 +0900

    Update client.h

--- client/client.h
+++ client/client.h
@@ -9,6 +9,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define RED 'R'
 #define BLUE 'B'
@@ -39,13 +40,15 @@ typedef struct
     int blue_tiles;
     int players_ready;
     int player_num;
+    pthread_mutex_t lock; // 뮤텍스 추가
 } GameInfo;
 
 void error_handling(char *message);
 void connect_to_server(const char *ip, int port, int *sock);
-void game_loop(int sock);
+void *game_loop(void *arg); // 스레드 함수 선언
 void print_board(GameInfo *game, int player_id);
 void send_command(int sock, char command);
 int receive_game_info(int sock, GameInfo *game);
+void *update_game_info_thread(void *arg); // 스레드 함수 선언
 
 #endif
